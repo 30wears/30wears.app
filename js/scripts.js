2a7a716a47f98f16cdd9b37a190dc9dc2d7f2fde
fiiv <git@fiiv.io>
2019-08-31 14:17:59 +0800

    new site updates

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+const responsive = selector => {
+  const elements = document.querySelectorAll(selector)
+
+  console.log(elements)
+
+  for (const el of elements) {
+    const parentWidth = Number(el.parentElement.offsetWidth)
+    const multiplier = (Number(el.dataset.widthPercent) || 100)
+
+    const oldWidth = Number(el.offsetWidth)
+    const oldHeight = Number(el.offsetHeight)
+    const ratio = (parentWidth * (multiplier / 100)) / oldWidth
+
+    // Resize it
+    el.width = parentWidth * (multiplier / 100)
+    el.height = ratio * oldHeight
+
+    // Center it
+    const margin = (100 - multiplier) / 2
+    el.style.marginLeft = margin + '%'
+    el.style.marginRight = margin + '%'
+  }
+}
+
+const init = () => {
+  responsive('.responsive')
+}
+
+init()
